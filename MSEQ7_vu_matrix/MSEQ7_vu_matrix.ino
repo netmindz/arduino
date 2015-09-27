aa9b963d9157ae3eeefec8cb3e6451493d93f886
Will Tatam <will.tatam@red61.com>
2015-09-27 20:41:12 +0100

    base vu from centre and make stereo

--- MSEQ7_vu_matrix/MSEQ7_vu_matrix.ino
+++ MSEQ7_vu_matrix/MSEQ7_vu_matrix.ino
@@ -3,19 +3,14 @@
  * Adafruit Microphone Amplifier
  ****************************************/
 
-#include <Wire.h>
 #include <FastLED.h>
-#include <Average.h>
-
-#define WIDTH 8
-#define HEIGHT 8
 
 #include "MSGEQ7.h"
 #define pinAnalogLeft A0
 #define pinAnalogRight A1
 #define pinReset 5
 #define pinStrobe 4
-#define MSGEQ7_INTERVAL ReadsPerSecond(10)
+#define MSGEQ7_INTERVAL ReadsPerSecond(20)
 #define MSGEQ7_SMOOTH true
 
 CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalogLeft, pinAnalogRight> MSGEQ7;
@@ -26,30 +21,22 @@ CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalogLeft, pinAnalogRight> MSGEQ
 #define COLOR_ORDER BGR
 #define CHIPSET     DOTSTAR
 
-#define BRIGHTNESS 20
+#define BRIGHTNESS 50
+#define DITHER 255 // try 0 to disable flickering
 
-#define WIDTH 15
-#define HEIGHT 7
-
-
-#define BUFFER_INTERVAL 10
+#define WIDTH 8
+#define HEIGHT 15
 
 CRGB leds[(WIDTH * HEIGHT)];
-Average<unsigned int> peaks(200);
 
 CRGB color;
 
-unsigned int sample;
-unsigned int maxPeak = 0;
-const boolean gay = true;
-const boolean runningGain = true;
-
-unsigned int bufferPos = 0;
-unsigned int runningMax = 0;
+const boolean gay = false;
 
 void setup() {
   Serial.begin(9600);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setDither(DITHER);
   FastLED.addLeds<DOTSTAR, LED_PIN, CLOCK_PIN, COLOR_ORDER>(leds, (WIDTH * HEIGHT)).setCorrection( TypicalLEDStrip );
 }
 
@@ -60,44 +47,55 @@ void loop() {
   // Led strip output
   if (newReading) {
 
+    int displayPeakL = map(MSGEQ7.get(MSGEQ7_LOW, 0), 0, 255, 0, round(HEIGHT / 2));
+    int displayPeakR = map(MSGEQ7.get(MSGEQ7_LOW, 1), 0, 255, 0, round(HEIGHT / 2));
+    Serial.print("Display peak: ");
+    Serial.println(displayPeakL);
 
-    // map 1v p-p level to the max scale of the display
-    int displayPeak = map(MSGEQ7.get(MSGEQ7_BASS), 0, 255, 0, HEIGHT);
-    //  Serial.print("Display peak: ");
-    //  Serial.println(displayPeak);
+    moveRight();
+    int offset = round(HEIGHT / 2);
 
-    moveLeft();
+    for (int i = 1; i <= HEIGHT; i++){
+        drawPixel(1, i, CRGB::Black);
+    }
+    
+    for (int i = 1; i <= HEIGHT; i++){
+        drawPixel(1, offset, CRGB::Blue);
+    }
 
-    // draw the new sample
-    for (int i = 1; i <= HEIGHT; i++)
-    {
-      if (i > displayPeak) // blank these pixels
-      {
-        drawPixel(i, 1, CRGB::Black);
+    for(int i=1; i <= displayPeakL; i++) {
+      if(gay) {
+         color = CHSV(map(i, 1, (HEIGHT/2), 0, 230), 255, 255);
       }
-      else
-      {
-
-        if (gay) {
-          unsigned int v = 254;
-          //        unsigned int v = map(displayPeak, 1, HEIGHT, 50, 254);
-          color = CHSV(map(i, 1, HEIGHT, 230, 0), 255, v);
-        }
-        else {
+      else {
           unsigned int g = map(i, 1, HEIGHT, 254, 0);
           color = CRGB(map(i , 1, HEIGHT, 0, 254), g, 0);
-        }
-
-        drawPixel(i, 1, color);
       }
+      drawPixel(1, (offset - i), color);
     }
+    for(int i=1; i <= displayPeakR; i++) {
+      if(gay) {
+         color = CHSV(map(i, 1, (HEIGHT/2), 230, 0), 255, 255);
+      }
+      else {
+          unsigned int g = map(i, 1, (HEIGHT/2), 254, 0);
+          color = CRGB(map(i , 1, (HEIGHT/2), 0, 254), g, 0);
+      }
+      drawPixel(1, (offset + i), color);
+    }
+      
     FastLED.show();
   }
 }
 
+/*
+1,1 = 0
+1,2 = 1
+2,1 = 15
+*/
 
 int xytopixel(int x, int y) {
-  int p = ((y - 1) * WIDTH) + (x - 1);
+  int p = ((x - 1) * HEIGHT) + (y - 1);
   return p;
 }
 
@@ -105,11 +103,11 @@ void drawPixel(int x, int y, CRGB color) {
   leds[xytopixel(x, y)] = color;
 }
 
-void moveLeft() {
+void moveRight() {
   // Update the display:
-  for (int i = HEIGHT; i > 1; i--) {
+  for (int i = HEIGHT; i >= 1; i--) {
     for (int j = WIDTH; j >= 1; j--) {
-      int src = xytopixel(j, (i - 1));
+      int src = xytopixel((j - 1), i);
       int dst = xytopixel(j, i);
       leds[dst] = leds[src];
     }
