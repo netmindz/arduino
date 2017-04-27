8ccd64bd9122f354df82a8fdbf1a6c1d87c587e7
Will Tatam <will@netmindz.net>
2017-04-27 15:37:38 +0100

    other: rewrite rainbowSweep to only do a single frame, not full itteration per call

--- Deevstock/DeevstockDMX/p-rainbowSweep.ino
+++ Deevstock/DeevstockDMX/p-rainbowSweep.ino
@@ -1,14 +1,22 @@
 int hue = 0;
+int rsX = 0;
+int jump = 0; 
 void rainbowSweep() {
-  int jump = random(0, map(STEPS, 0, 255, 0, 25)); // TWEAK ME
-  int hue = random(0, 255); // Audio ?
-  for (int x = 0; x < kMatrixWidth; x++) {
+  
+  
     for (int y = 0; y < kMatrixHeight; y++) {
-      leds[ XY(x, y)]  = CHSV( hue, 255, 255);
+      leds[ XY(rsX, y)]  = CHSV( hue, 255, 255);
     }
-    FastLED.delay(map(SPEEDO,0,255, 0, 1000));  // TWEAK ME was 70
-    hue += jump;
-    if (hue > 255) hue = 0;
-    fadeToBlackBy(leds, NUM_LEDS, map(STEPS,0,255, 0, 100));
-  }
+    FastLED.delay(map(SPEEDO,0,255, 200, 1));  // TWEAK ME was 70
+    fadeToBlackBy(leds, NUM_LEDS, map(FADE,0,255, 100, 5));
+    if(rsX < kMatrixWidth) {
+      rsX++;
+      hue += jump;
+      if (hue > 255) hue = 0;
+    }
+    else {
+      rsX = 0;
+      hue = random(0, 255); // Audio ?
+      jump = random(0, map(STEPS, 0, 255, 0, 100)); // TWEAK ME
+    }
 }
