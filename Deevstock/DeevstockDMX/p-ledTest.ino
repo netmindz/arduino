68b3c106eb4a5b4bd3dac386874dfcf6cf93e13f
Will Tatam <will@netmindz.net>
2019-01-20 15:28:49 +0000

    Updated led test

--- Deevstock/DeevstockDMX/p-ledTest.ino
+++ Deevstock/DeevstockDMX/p-ledTest.ino
@@ -3,12 +3,19 @@ void ledtest() {
   for (int y = 0; y < kMatrixHeight; y++) {
     for (int x = 0; x < kMatrixWidth; x++) {
       leds[ XY(x, y)]  = CHSV( hue, 255, 255);
-      FastLED.delay(100);
+      FastLED.delay(50);
       leds[ XY(x, y)]  = CRGB::Black;
       hue += 5;
       if (hue > 255) hue = 0;
     }
   }
+//  for (int i = 0; i < NUM_LEDS; i++) {
+//      leds[i]  = CHSV( 100, 255, 255);
+//      FastLED.delay(100);
+//      leds[i]  = CRGB::Black;
+//      hue += 5;
+//      if (hue > 255) hue = 0;
+//  }
 }
 
 
