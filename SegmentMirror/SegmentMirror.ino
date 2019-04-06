655d6de26613fd14332b9db33871ec83a73c0c27
Will Tatam <will@netmindz.net>
2019-04-06 18:24:04 +0100

    Final version of SegmentMirror test - where we want to copy a given set of LEDs to much larger set - useful for Audio stuff

--- SegmentMirror/SegmentMirror.ino
+++ SegmentMirror/SegmentMirror.ino
@@ -1,8 +1,10 @@
 #include <FastLED.h>
 
-#define NUM_LEDS_PER_STRIP 100
+#define NUM_LEDS_PER_STRIP 50
 #define NUM_STRIPS 8
-#define LED_TYPE WS2812B
+const uint8_t kMatrixWidth = 50; // length of string
+const uint8_t kMatrixHeight = 16; // number of strings
+#define LED_TYPE WS2811
 #define LED_PIN D4
 #define COLOR_ORDER BGR
 
@@ -14,8 +16,8 @@ CRGB leds[NUM_LEDS];
 const bool kMatrixSerpentineLayout = true;
 
 void setup() {
-//  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  //  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
 }
 
 void loop() {
@@ -38,9 +40,36 @@ void showSegments() {
   for (int s = 0; s < NUM_STRIPS; s++) {
     for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
       int p = (s * NUM_LEDS_PER_STRIP) + i;
-      leds[p] = ledSegment[i];
+      leds[getPixel(p)] = ledSegment[i];
     }
   }
-  FastLED.show();
+  FastLED.delay(1000);
 }
+
+
+uint16_t getPixel(int p)
+{
+  uint16_t i;
+
+  if ( kMatrixSerpentineLayout == false) {
+    i = p;
+  }
+
+  if ( kMatrixSerpentineLayout == true) {
+    float f = ((float)(p + 1) / (float) kMatrixWidth);
+    int r = ceil(f);
+
+    if (r & 0x01)   {
+      i = p;
+    } else {
+      i = ((kMatrixWidth * r) - (p % kMatrixWidth)) - 1;
+    }
+  }
+
+  if (i >= (NUM_LEDS - 1)) return 0;
+  return i;
+}
+
+
+
 
