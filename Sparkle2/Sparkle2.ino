fb086d66eb60482071e214ef714fce394e9cb552
Will Tatam <will@netmindz.net>
2019-04-06 18:50:29 +0100

    Move pattern to its own file

--- Sparkle2/Sparkle2.ino
+++ Sparkle2/Sparkle2.ino
@@ -9,46 +9,7 @@
 
 CRGB leds[NUM_LEDS];      //naming our LED array
 
-class Star {
-    int p = 0;
-    int dir = 0;
-    uint8_t hue = random(0, 255);
-    int v = 0;
-    int vary = random(1, 5);
-    int r = 15;
-
-  public:
-    Star() {
-
-    }
-
-    void setPixel(int pixel) {
-      p = pixel;
-    }
-
-    void sparkle() {
-      // should we change colour?
-      if (random(1, r) == 1) {
-        if (dir > 0) {
-          hue += vary;
-        }
-        else {
-          hue -= vary;
-        }
-        // should we change direction?
-        if (random(1, 25) == 1) {
-          dir = random(0, 1);
-          if(v == 0) v = 255;
-        }
-      }
-      if(v >= 1) v--;
-      leds[p]  = CHSV(hue, 255, v);
-    }
-};
-
-
-
-Star stars[NUM_LEDS];
+#include "stars.h"
 
 void setup() {
 //  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
@@ -63,19 +24,11 @@ void setup() {
 }
 
 void loop() {
-  sparkle2();
+  showStars();
 }
 
 int p = 0;
 int hue = 0;
 
-
-void sparkle2() {
-
-  for (int  i = 0; i < NUM_LEDS; i++) {
-    stars[i].sparkle();
-  }
-  FastLED.delay(0);
-}
 
 
