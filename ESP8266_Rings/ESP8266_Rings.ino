9eac4e885204ff30ac7d7a4078c567412381e5c4
Will Tatam <git@netmindz.net>
2020-12-30 23:02:53 +0000

    Add OTA support

--- ESP8266_Rings/ESP8266_Rings.ino
+++ ESP8266_Rings/ESP8266_Rings.ino
@@ -6,6 +6,7 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncE131.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #define UNIVERSE 1        // First DMX Universe to listen for
 #define UNIVERSE_COUNT 1  // Total number of Universes to listen for, starting at UNIVERSE
@@ -96,9 +97,43 @@ void setup() {
 
   fftUdp.beginMulticast(WiFi.localIP(), IPAddress(239, 0, 0, 1), audioSyncPort);
 
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
 }
 
 void loop() {
+  ArduinoOTA.handle();
   if (!e131.isEmpty()) {
     e131_packet_t packet;
     e131.pull(&packet);     // Pull packet from ring buffer
