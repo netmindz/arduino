6e03cf06bc5a1438b8224ad78acfb63847ca5744
Will Tatam <will@netmindz.net>
2021-09-19 11:36:49 +0100

    Add video and readme

--- Deevstock/CloudsV3/CloudsV3.ino
+++ Deevstock/CloudsV3/CloudsV3.ino
@@ -1,3 +1,7 @@
+// E1.31 Fixture for lightning clouds 
+// https://youtu.be/QarQ4QX6d04
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+
 #define FASTLED_ALLOW_INTERRUPTS 0
 
 #include<FastLED.h>
---
+++ Deevstock/CloudsV3/README.md
@@ -0,0 +1,5 @@
+E1.31 Fixture for lightning clouds 
+
+https://youtu.be/QarQ4QX6d04
+
+https://creativecommons.org/licenses/by-nc-sa/4.0/
