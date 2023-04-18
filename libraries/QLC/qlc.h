d7bfa679378a715efeb371285cd856609e76ff1a
Will Tatam <will@netmindz.net>
2023-04-18 11:39:01 +0100

    Add color to fixture

--- libraries/QLC/qlc.h
+++ libraries/QLC/qlc.h
@@ -1,13 +1,13 @@
-void qlcDumpDef(PatternAndNameList patterns, PaletteAndNameList palettes) {
+void qlcDumpDef() {
 
-  int pcount = ARRAY_SIZE(patterns);
-  int addrMin = 0;
+  uint8_t pcount = ARRAY_SIZE(gPatterns);
+  uint8_t addrMin = 0;
   uint8_t width = (255 / pcount);
 
   Serial.println(" <Channel Name=\"Pattern\">");
   Serial.println("  <Group Byte=\"0\">Gobo</Group>");
   for(int p = 0; p < pcount; p++) {
-    Serial.printf("   <Capability Min=\"%u\" Max=\"%u\">%s</Capability>\n", addrMin, (addrMin + width), patterns[p].name.c_str());
+    Serial.printf("  <Capability Min=\"%u\" Max=\"%u\">%s</Capability>\n", addrMin, (addrMin + width), gPatterns[p].name.c_str());
     addrMin += width + 1;
   }
   Serial.println(" </Channel>");
@@ -15,8 +15,10 @@ void qlcDumpDef(PatternAndNameList patterns, PaletteAndNameList palettes) {
   Serial.println(" <Channel Name=\"Palette\">");
   Serial.println("  <Group Byte=\"0\">Colour</Group>");
   for(int p = 0; p < ARRAY_SIZE(palettes); p++) {
-    Serial.printf("   <Capability Min=\"%u\" Max=\"%u\">%s</Capability>\n", addrMin, (addrMin + width), palettes[p].name.c_str());
-    addrMin += width + 1;
+    CRGB color = ColorFromPalette(palettes[p].palette, 125);
+    char hexout[8];
+    sprintf(hexout,"#%02x%02x%02x",color.r, color.g, color.b);
+    Serial.printf("  <Capability Min=\"%u\" Max=\"%u\" Preset=\"ColorMacro\" Res1=\"%s\">%s</Capability>\n", p, p, hexout, palettes[p].name.c_str());
   }
   Serial.println(" </Channel>");
 
